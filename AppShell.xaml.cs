5300caf5a5d675385765d40d73480ed74b8887ac
Alberto <vazquez26fig@gmail.com>
2025-09-18 13:50:46 -0600

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LugaresVisitados
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LugaresVisitados")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LugaresVisitados.Pages")]
